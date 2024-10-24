4c36aebb69ada8fbebeb397c907f1ec952d0c00f
Thammaruthai <thammaruthai@gmail.com>
2024-10-24 13:44:02 +0700

    feat: Answer Array Handling 1

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,7 +1,37 @@
+import { useState } from "react";
+
 function MessageBoard() {
+  //state
+  const [inputMsg, setInputMsg] = useState("");
+  const [messageAdd, setMessageAdd] = useState([
+    "Hello all ! This is first message.",
+  ]);
+
+  //handling event
+  const handlingInputMsg = (event) => {
+    setInputMsg(event.target.value);
+  };
+
+  const handlingMessageAdd = () => {
+    const newMessage = [...messageAdd, inputMsg];
+    setMessageAdd(newMessage);
+    setInputMsg("");
+    console.log(messageAdd);
+  };
+
+  const handlingMessageDel = (index) => {
+    const newMessage = [...messageAdd].filter(
+      (filtervalue, filterindex) => index !== filterindex
+    );
+    setMessageAdd(newMessage);
+    console.log(messageAdd);
+  };
+
+ 
+
   return (
     <div className="app-wrapper">
-      <h1 class="app-title">Message board</h1>
+      <h1 class="app-title">Message board {inputMsg}</h1>
       <div class="message-input-container">
         <label>
           <input
@@ -9,18 +39,35 @@ function MessageBoard() {
             name="message-text"
             type="text"
             placeholder="Enter message here"
+            value={inputMsg}
+            onChange={handlingInputMsg}
           />
         </label>
-        <button className="submit-message-button">Submit</button>
+        <button className="submit-message-button" onClick={handlingMessageAdd}>
+          Submit
+        </button>
       </div>
       <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
-        </div>
+        {messageAdd.map((key, index) => {
+          return (
+            <div className="message" key={index}>
+              <h1 key={index}>{key}</h1>
+              <button
+                className="delete-button"
+                key={index}
+                onClick={() => {
+                  handlingMessageDel(index);
+                }}
+              >
+                x
+              </button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
 }
 
 export default MessageBoard;
+          
